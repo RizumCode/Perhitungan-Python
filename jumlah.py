0f82c0acfadfd29c0e9c33319a780ad5156f5c47
Muhamad Rizqi Habibbullah <46855186+RizumCode@users.noreply.github.com>
2019-01-20 03:29:54 +0000

    Add files via upload

---
+++ jumlah.py
@@ -0,0 +1,11 @@
+# Program Penjumlahan Dua Bilangan 
+# Meminta inputan dari user 
+bil1 = input('Masukkan bilangan pertama: ') 
+bil2 = input('Masukkan bilangan kedua: ')
+bil3 = input('Masukan bilangan ketiga: ')
+bil4 = input('Masukan bilangan keempat: ')
+bil5 = input ('Masukan bilangan kelima: ')
+# Menjumlahkan bilangan 
+jumlah = float(bil1) + float(bil2) - float(bil3) / float(bil4) * float(bil5)
+# Menampilkan jumlah 
+print('Jumlah {0} + {1} - {2} / {3} * {4} adalah {5}'.format(bil1, bil2, bil3, bil4, bil5, jumlah))
